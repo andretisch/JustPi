7677a59108d09c4c469a18e0d1c313a3eddec9f7
Andrey Tishkin <48943433+andretisch@users.noreply.github.com>
2021-03-21 03:59:54 +0300

    Create justpi.py

---
+++ justpi.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import timeit
+
+code_to_test = """
+from decimal import Decimal, getcontext
+getcontext().prec=10000000
+sum(1/Decimal(16)**k * 
+          (Decimal(4)/(8*k+1) - 
+           Decimal(2)/(8*k+4) - 
+           Decimal(1)/(8*k+5) -
+           Decimal(1)/(8*k+6)) for k in range(100))
+"""
+
+elapsed_time = timeit.timeit(code_to_test,number=1)
+print(elapsed_time)
