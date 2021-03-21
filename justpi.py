e4bcf360a6cd2e732bbec2c13287264d01ac2715
Andrey Tishkin <48943433+andretisch@users.noreply.github.com>
2021-03-21 04:05:56 +0300

    Update justpi.py

--- justpi.py
+++ justpi.py
@@ -11,7 +11,6 @@ sum(1/Decimal(16)**k *
            Decimal(1)/(8*k+5) -
            Decimal(1)/(8*k+6)) for k in range(100))
 """
-
+print('Start count 10M in seconds:')
 elapsed_time = timeit.timeit(code_to_test,number=1)
-print('Start count 10M in secomds:')
 print(elapsed_time)
